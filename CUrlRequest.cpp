2883b39daafb1b27ae43877856b3dd08841c906e
khan.lau <khan.lau@hotmail.com>
2016-09-21 10:39:15 +0800

    Update CUrlRequest.cpp
    support post application/json

--- CUrlRequest.cpp
+++ CUrlRequest.cpp
@@ -208,6 +208,91 @@ void CUrlRequest::post(string url, const Headers & headers, const Parameters & p
 }
 
 
+void CUrlRequest::post(string url, const string &json, function<void(const char*body, size_t len)> func_finish, function<void(const int code)> failt_callback) {
+	this->post(url, Headers(), json, func_finish, failt_callback);
+}
+
+void CUrlRequest::post(string url, const Headers &headers, const string &json, function<void(const char*body, size_t len)> func_finish, function<void(const int code)> failt_callback) {
+	vector<char> data;
+	CURL * curl = _pCUrl.get();
+	if (curl != NULL) {
+
+		struct curl_slist * headerlist = NULL;
+
+		headerlist = curl_slist_append(headerlist, "Content-Type:application/json;charset=UTF-8");
+
+		for (string header : headers) {
+			headerlist = curl_slist_append(headerlist, header.c_str());
+		}
+
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json.c_str()); // 设置要POST的JSON数据  
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);//设置CURLOPT_WRITEFUNCTION的最后一个参数值
+
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, static_cast<CURL_WRITEFUNCTION_PTR>([](void *buffer, size_t size, size_t count, void *data)->size_t {
+			size_t n = size * count;
+			vector<char> *vec = (vector<char> *)data;
+			char * pstr = (char*)buffer;
+
+			std::copy(pstr, pstr + n, std::back_inserter(*vec));
+			TRACE("CURL_WRITEFUNCTION_PTR\n");
+			return n;
+		}));
+
+		if (this->_cookie_file != "") {
+			curl_easy_setopt(curl, CURLOPT_COOKIESESSION, 1L);
+			curl_easy_setopt(curl, CURLOPT_COOKIEFILE, this->_cookie_file);
+		} else {
+			curl_easy_setopt(curl, CURLOPT_COOKIESESSION, 0L);
+		}
+
+
+		string tmp;
+		string https("https");
+		std::transform(url.begin(), url.end(), std::back_inserter(tmp), tolower);
+
+		if (tmp.compare(0, https.size(), https) == 0) {
+			if (this->_ca_path != "" && this->_ca_info != "") {
+				curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+
+				curl_easy_setopt(curl, CURLOPT_CAPATH, this->_ca_path);
+				curl_easy_setopt(curl, CURLOPT_CAINFO, this->_ca_info);
+
+				curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L);
+			} else {
+				curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+				curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+			}
+
+		}
+
+		//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 30);
+
+		_res = curl_easy_perform(curl);
+
+		if (_res != CURLE_OK) {
+			failt_callback(_res);
+
+			//TRACE(L"%S\n", to_wchar_t(curl_easy_strerror(_res)));
+
+		} else {
+			long http_code = 0;
+			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+			if (http_code == 200 || http_code == 304) {
+				string str(data.begin(), data.end());
+				func_finish(str.c_str(), str.length());
+			} else {
+				failt_callback(http_code);
+			}
+		}
+		if (headerlist != NULL) curl_slist_free_all(headerlist);
+	}
+}
+
+
+
 CUrlRequest::~CUrlRequest()
 {
 }
